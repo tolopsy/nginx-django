784f5afb6326d8dde00a21e3aa59d6cd49c68254
Tolulope Olanrewaju <lanre.toluwa@gmail.com>
2023-05-11 21:08:25 +0100

    demonstrating basic http auth with htpasswd and nginx

--- django/demo/urls.py
+++ django/demo/urls.py
@@ -6,5 +6,6 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('ping/', TemplateView.as_view(template_name="ping.html")),
+    path('secure/', TemplateView.as_view(template_name="secure.html")),
     path('', debug.default_urlconf),
 ]
